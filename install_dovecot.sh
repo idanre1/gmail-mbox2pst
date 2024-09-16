68391922dfb4276a13e61a8c62f5d09cc987de6f
Idan Regev <idanre1@gmx.com>
2024-09-16 19:13:26 +0300

    mbox documentation

--- install_dovecot.sh
+++ install_dovecot.sh
@@ -2,5 +2,6 @@
 sudo apt update
 sudo apt install dovecot-imapd dovecot-pop3d
 sudo systemctl stop dovecot
+# https://doc.dovecot.org/2.3/configuration_manual/mail_location/mbox/
 sudo sh -c "echo 'mail_location = mbox:~/mail' >> /etc/dovecot/dovecot.conf"
 mkdir -p ~/mail
