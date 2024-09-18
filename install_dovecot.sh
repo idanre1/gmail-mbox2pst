c2a8890bb40fce13ca70f378a0b7ee5aca42cfe4
Idan Regev <idanre1@gmail.com>
2024-09-18 14:50:02 +0300

    Update install_dovecot.sh

--- install_dovecot.sh
+++ install_dovecot.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt update
 sudo apt install dovecot-imapd dovecot-pop3d
+sudo systemctl disable dovecot
 sudo systemctl stop dovecot
 # https://doc.dovecot.org/2.3/configuration_manual/mail_location/mbox/
 sudo sh -c "echo 'mail_location = mbox:~/mail' >> /etc/dovecot/dovecot.conf"
